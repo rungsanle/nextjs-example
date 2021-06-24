8248ed0f52d06186256a1bf6bbeae16027c796d9
rungsanle <rungsan.le@gmail.com>
2021-06-24 15:44:29 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+module.exports = {
+  reactStrictMode: true,
+  trailingSlash: true,
+	webpackDevMiddleware: config => {
+		config.watchOptions = {
+			poll: 1000,
+			aggregateTimeout: 300
+		}
+
+		return config
+	},
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')]
+	},
+	images: {
+		domains: [
+		  'dummyimage.com'
+		]
+	}
+}
